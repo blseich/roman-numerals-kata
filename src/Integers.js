2b100bc5fc5545020b0d71fa41c74612ab5d7d43
morales201 <morales.201@osu.edu>
2018-07-23 22:12:55 -0400

    Created a working program that converts integers into numerals

--- src/Integers.js
+++ src/Integers.js
@@ -1,27 +1,64 @@
-const createOnesArray = () => {
-    return [[],[1], [1,1], [1,1,1], [1,5], [5], [5,1], [5,1,1], [5,1,1,1], [1,10]];
+import { curry } from 'lodash';
+
+const ones = {
+    '0': '',
+    '1': 'I',
+    '5': 'V',
+    '10': 'X'
 };
 
-const getOnesPlace = (integer) => {
-    const ones = {
-        1: 'I',
-        5: 'V',
-        10: 'X'
-    };
-    
-    return createOnesArray()[integer].map( curr => ones[curr]).join('');
-}
+const tens = {
+    '0': '',
+    '1': 'X',
+    '5': 'L',
+    '10': 'C'
+};
+
+const hundreds = {
+    '0': '',
+    '1': 'C',
+    '5': 'D',
+    '10': 'M'
+};
+
+const thousands = {
+    '1': 'M'
+};
+
+const patternArray = [['0'],['1'], ['1','1'], ['1','1','1'], ['1','5'], ['5'], ['5','1'], ['5','1','1'], ['5','1','1','1'], ['1','10']];
+
+const basePatternArrayReplacerFunction = (placeObject, intLessThanTen) => {
+    return patternArray[intLessThanTen].map( curr => placeObject[curr]).join('');
+};
+
+const createPatternArrayReplacerFunction = curry(basePatternArrayReplacerFunction);
+
+const replaceOnesPlace = createPatternArrayReplacerFunction(ones);
+const replaceTensPlace = createPatternArrayReplacerFunction(tens);
+const replaceHundredsPlace = createPatternArrayReplacerFunction(hundreds);
+const replaceThousandsPlace = (intLessThanTen) => {
+    if(intLessThanTen === '4'){
+        return 'MMMM';
+    } else {
+        return patternArray[intLessThanTen].map( curr => thousands[curr]).join('');
+    }
+};
+const replacerFunctions = [replaceThousandsPlace, replaceHundredsPlace, replaceTensPlace, replaceOnesPlace];
+
+const convertIntToArray = (integer) => {
+    return integer.toString().split('');
+};
 
 const integerToNumeral = (integer) => {
-    return getOnesPlace(integer);
-}
+    let intArr = convertIntToArray(integer);
+    let replacerFunctionsCopy = replacerFunctions.slice(); 
+
+    return intArr.reduceRight((numeral, intLessThanTen) => {
+        let replacer = replacerFunctionsCopy.pop();
+        return replacer(intLessThanTen) + numeral;
+    }, '');
+};
 
 export {
     integerToNumeral
 };
-
-// IVXLCDM
-// ones: IVX
-// tens: XLC
-// hundreds: CDM
-// thousands: M
--- tests/integer-to-numeral-spec.js
+++ tests/integer-to-numeral-spec.js
@@ -26,4 +26,63 @@ describe('Converting Integers to Roman Numerals', () => {
         expect(integerToNumeral(9)).to.be.equal('IX');
     });
 
+    it('should convert 10 to X', () => {
+        expect(integerToNumeral(10)).to.be.equal('X');
+    });
+
+    it('should convert 38 to XXXVIII', () => {
+        expect(integerToNumeral(38)).to.be.equal('XXXVIII');
+    });
+
+    it('should convert 49 to XLIX', () => {
+        expect(integerToNumeral(49)).to.be.equal('XLIX');
+    });
+
+    it('should convert 50 to L', () => {
+        expect(integerToNumeral(50)).to.be.equal('L');
+    });
+
+    it('should convert 88 to LXXXVIII', () => {
+        expect(integerToNumeral(88)).to.be.equal('LXXXVIII');
+    });
+
+    it('should convert 99 to XCIX', () => {
+        expect(integerToNumeral(99)).to.be.equal('XCIX');
+    });
+
+    it('should convert 100 to C', () => {
+        expect(integerToNumeral(100)).to.be.equal('C');
+    });
+
+    it('should convert 253 to CCLIII', () => {
+        expect(integerToNumeral(253)).to.be.equal('CCLIII');
+    });
+
+    it('should convert 499 to CDXCIX', () => {
+        expect(integerToNumeral(499)).to.be.equal('CDXCIX');
+    });
+
+    it('should convert 500 to D', () => {
+        expect(integerToNumeral(500)).to.be.equal('D');
+    });
+
+    it('should convert 749 to DCCXLIX', () => {
+        expect(integerToNumeral(749)).to.be.equal('DCCXLIX');
+    });
+
+    it('should convert 999 to CMXCIX', () => {
+        expect(integerToNumeral(999)).to.be.equal('CMXCIX');
+    });
+
+    it('should convert 1000 to M', () => {
+        expect(integerToNumeral(1000)).to.be.equal('M');
+    });
+
+    it('should convert 2166 to MMCLXVI', () => {
+        expect(integerToNumeral(2166)).to.be.equal('MMCLXVI');
+    });
+
+    it('should convert 4999 to MMMMCMXCIX', () => {
+        expect(integerToNumeral(4999)).to.be.equal('MMMMCMXCIX');
+    });
 });
