f8e7621e4efe05451ef8edad73c270dcfc628fcc
Brandon Seich (br5g8ch) <brandon_seich@gap.com>
2021-03-26 11:01:41 -0400

    convert to jest

--- tests/numeral-to-integer.spec.js
+++ tests/numeral-to-integer.spec.js
@@ -1,10 +1,9 @@
-import { expect } from 'chai'
 import { numeralToInteger } from '../src/RomanNumerals'
 
 describe('Converting Roman Numerals to Integers', () => {
     
     it('should convert I to 1', () => {
-        expect(numeralToInteger('I')).to.be.equal(1)
+        expect(numeralToInteger('I')).toEqual(1)
     })
     
 })
